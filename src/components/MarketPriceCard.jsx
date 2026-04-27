bdf44100ff25abbe1052a116e0a9790604e655b1
Tavan Mohammed <tavan.mohammed19@gmail.com>
2026-04-26 22:34:11 -0400

    Refactor MarketPriceCard for better responsiveness
    Updated the MarketPriceCard component to improve layout and responsiveness.

--- src/components/MarketPriceCard.jsx
+++ src/components/MarketPriceCard.jsx
@@ -22,8 +22,8 @@ function MarketPriceCard({ title, subtitle, price, symbol, data }) {
         </div>
       </div>
 
-      <div className="h-48">
-        <ResponsiveContainer width="100%" height="100%">
+      <div className="w-full h-48 min-w-0">
+      <ResponsiveContainer width="100%" height={192}>
           <AreaChart data={data}>
             <defs>
               <linearGradient id={`${title}-gradient`} x1="0" y1="0" x2="0" y2="1">
